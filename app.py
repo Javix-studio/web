fa1a8fa043366cf59db625387be8b8082721e22d
Durai1759 <rajadurai14008@gmail.com>
2025-05-19 15:26:26 +0530

    Changes to be committed:

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 app.register_blueprint( view ,url_prefix="/")
 
 
-#if __name__ == '__main__' :
- #   app.run(debug=True)
+#if __name__ == '__main__' : 
+    #app.run(debug=True)
