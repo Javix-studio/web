9ba36fead90ea64162b9587ddb11c074e98cb150
RIVAX STUDIO <93965801+Javix-studio@users.noreply.github.com>
2025-05-19 11:40:59 +0530

    Add files via upload

---
+++ static/js/contact.js
@@ -0,0 +1,6 @@
+function sendEmail() {
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  console.log('Email:', email);
+  console.log('Message:', message);
+}
