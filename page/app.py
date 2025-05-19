f4dfdb60a1df645bec4b097e831ea9d4a24fa1b4
RIVAX STUDIO <93965801+Javix-studio@users.noreply.github.com>
2025-05-19 11:40:05 +0530

    Delete page directory

--- page/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from view import view
-
-app = Flask(__name__)
-app.register_blueprint( view ,url_prefix="/")
-
-
-if __name__ == '__main__' :
-    app.run(debug=True)
--- page/static/js/contact.js
+++
@@ -1,6 +0,0 @@
-function sendEmail() {
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-  console.log('Email:', email);
-  console.log('Message:', message);
-}
